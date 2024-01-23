2b2576b16626ee9f9883f225e5d1c2043215ca64
Aruka <aruka16661@gmail.com>
2024-01-23 15:34:02 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dBContext;
+        public ApiController(MyDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+
+            var products = _dBContext.Addresses.Select(p => p.City).Distinct();
+
+            return Json(products);
+        }
+    }
+}
---
+++ WebApplication1/README.md
@@ -0,0 +1,2 @@
+# 20240122
+### 請填寫駔業的GitHub連接 [https://github.com/ArukaTarasi/20240122.solution.git]
