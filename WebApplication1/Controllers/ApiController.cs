dd8f5f53a26540fe2dfec919909a56dd7ffbe37e
Aruka <aruka16661@gmail.com>
2024-01-24 02:03:55 +0800

    課程檔案更新
    實作：
    資料解構
    完成作業1，並優化排版
    
    實作：
    1.fetch
    2.讀取&取消讀取
    3.將值放入Selector並取得DataBase表格的值轉成Json用立即執行函式取得並放入

--- WebApplication1/Controllers/ApiController.cs
+++ WebApplication1/Controllers/ApiController.cs
@@ -11,16 +11,62 @@ namespace WebApplication1.Controllers
         {
             _dBContext = dBContext;
         }
+
         public IActionResult Index()
         {
-            return View();
+            // 延遲5秒
+            System.Threading.Thread.Sleep(5000);
+
+            //return View();
+            //return Json("Hello Content"); // 回傳純文字
+
+            // 連線至API /api/index
+            // 傳入文字的第二個參數是用什麼格式的資料
+            // 傳入文字的第三個參數是用什麼方式做編碼
+            // return Content("1","2",3);
+            // return Content("<h2>Hello Content</h2>"); // 還傳HTML標籤，沒有意義 會還傳整段文字
+            // return Content("<h2>Hello Content 你好</h2>", "text/plain"); // plain 純文字格式，插入中文會變成亂碼
+            return Content("Content 你好", "text/plain", System.Text.Encoding.UTF8); // 加上編碼方式
+            // return Content("<h2>Hello Content</h2>", "text/html"); // 給第二個參數 "text/html" 指定格式就能使用HTML
+
+            // return Content(new { name = "Hello Content" }); // 回傳物件
         }
+
+        //[HttpPost]// HttpPost 就無法使用API測試
         public IActionResult Cities()
         {
 
             var products = _dBContext.Addresses.Select(p => p.City).Distinct();
-
             return Json(products);
+            //return Json(products);
+        }
+
+        public IActionResult Districts(string city)
+        {
+            //根據City，取得不重複的 SiteId 放入 districts
+            var districts = _dBContext.Addresses.Where(p => p.City == city).Select(p => p.SiteId).Distinct();
+            return Json(districts);
+        }
+
+        public IActionResult Avatar(int id = 1)
+        {
+            Member? member = _dBContext.Members.Find(id);
+
+            if (member != null)
+            {
+                byte[]? img = member.FileData;
+                return File(img, "image/jpeg");
+            }
+            return NotFound();
+        }
+
+        public IActionResult Register(string name,int age = 20)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Guest";
+            }
+            return Content($"Hello {name} m You are {age} years old");
         }
     }
 }
